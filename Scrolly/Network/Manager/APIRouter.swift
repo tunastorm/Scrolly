a2ee0e50a8f26a3e104cc0a03614e57918ab4f26
tunastorm <ycw921@naver.com>
2024-08-14 18:45:10 +0900

    Update: Network 폴더링 및 오픈소스 라이브러리 의존성 설정

---
+++ Scrolly/Network/Manager/APIRouter.swift
@@ -0,0 +1,8 @@
+//
+//  APIRouter.swift
+//  Scrolly
+//
+//  Created by 유철원 on 8/14/24.
+//
+
+import Foundation
---
+++ Scrolly/Network/Manager/TargetType.swift
@@ -0,0 +1,8 @@
+//
+//  TargetType.swift
+//  Scrolly
+//
+//  Created by 유철원 on 8/14/24.
+//
+
+import Foundation
